d5b781071f2acb82d9ea8fb561a9af0cf0838494
vate <2324757255@qq.com>
2017-09-22 13:01:35 +0800

    增加url encode 组件

--- src/components/url/encode.vue
+++ src/components/url/encode.vue
@@ -1,24 +1,24 @@
 <template>
 
 <div class="mdui-container doc-container doc-no-cover pdl240">
-	<h1 class="doc-title mdui-text-color-theme">URL encode</h1>
+	<h1 class="doc-title mdui-text-color-theme">URL encode/decode</h1>
 	<div class="content">
 		<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-5 mdui-col-lg-5 mdui-col-xl-5">
 			<div class="encode">
-				<textarea class="encode">
+				<textarea ref='encode'>
 					
 				</textarea>
 			</div>
 		</div>
 		<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-2 mdui-col-lg-2 mdui-col-xl-2 w2">
 			<div class="change-button">
-				<button class="mdui-btn mdui-color-theme-accent mdui-ripple">UrlEncode编码</button>
-				<button class="mdui-btn mdui-color-theme-accent mdui-ripple">UrlDecode解码</button>
+				<button class="mdui-btn mdui-color-theme-accent mdui-ripple" @click="urlEncode()">编码<i class="mdui-icon material-icons">arrow_forward</i></button>
+				<button class="mdui-btn mdui-color-theme-accent mdui-ripple" @click="urlDecode()"><i class="mdui-icon material-icons">arrow_back</i>解码</button>
 			</div>
 		</div>
 		<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-5 mdui-col-lg-5 mdui-col-xl-5">
-			<div class="uncode">
-				<textarea class="encode">
+			<div class="encode">
+				<textarea ref='decode'>
 					
 				</textarea>
 			</div>
@@ -31,36 +31,51 @@ import time from '../../fetch/time.js'
 export default {
 	name: 'url',
 	mounted () {  
-    	document.title="URL encode";
+    	document.title="URL encode/decode";
+    	this.$refs.encode.value = this.encode; 
+    	this.$refs.decode.value = this.decode;
   	},
  	data () {
 	    return {
-	      
+	      encode:'木木工具',
+	      decode:'%e6%9c%a8%e6%9c%a8%e5%b7%a5%e5%85%b7'
 	    }
   	},
   	methods:{
-  		
+  		urlEncode()
+  		{
+  			this.$refs.decode.value = encodeURI(this.$refs.encode.value);
+  		},
+  		urlDecode()
+  		{
+  			this.$refs.encode.value = decodeURI(this.$refs.decode.value);
+  		}
   	}
 }
 </script>
 <style>
 .content{
-	text-align: center;
+	/* text-align: center; */
 }
 .w2{
-
-	margin: 0 auto;
+	/* margin: 0 auto; */
+	/* border: 1px solid red; */
 }
 .encode{
-	width: 98%;
+	width: 100%;
 	height: 300px;
+}
+.encode textarea{
+	width: 100%;
+	height: 100%;
 }
 .mdui-btn{
 	margin-bottom: 30px;
 }
 .change-button{
-	padding: 100px 0;
-	/* margin: 100px 0; */
+	text-align: center;
+	padding: 100px 10px;
+	
 }
 @media screen and (max-width: 1024px) {
     .change-button{
