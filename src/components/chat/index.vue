7549decdb436c989eb01a407e7d6034d758ee3aa
vate <2324757255@qq.com>
2017-09-26 09:59:48 +0800

    加入message图标

--- src/components/chat/index.vue
+++ src/components/chat/index.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="chat">
-	 <a href="javascript:void(0)" @click="showCen" v-if="isShowChatNum"><div class="chat-in mdui-color-theme-accent">
-	 	
-	 </div>
+	 <a href="javascript:void(0)" @click="showCen" v-if="isShowChatNum">
+     <div class="chat-in mdui-color-theme-accent">
+        <i class="mdui-icon material-icons">message</i>
+  	 </div>
    </a>
 	<chat-cent :show="isShowChatCen" :button="isShowChatNum" @change="change"></chat-cent>
 </div>
@@ -41,8 +42,10 @@ export default {
   bottom: 0px;
   right: 20px;
   width: 50px;
-  height: 20px;
+  height: 25px;
+  padding-top: 5px;
   border-top-right-radius: 10px;
   border-top-left-radius: 10px;
+  text-align: center;
 }
 </style>
