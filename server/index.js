5e182179c86ff28b9aa9e62ed0990d07ce61cd0f
null <null@null.null>
2017-10-13 11:57:13 +0800

    add email

--- server/index.js
+++ server/index.js
@@ -4,6 +4,14 @@ const express = require('express')
 const app     = express()
 const email   = require('./lib/email')
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
+
 app.get('/send-mail',(req,res) => {
 
    if(!req.query.to || req.query.title === undefined || req.query.content === undefined){
@@ -13,9 +21,9 @@ app.get('/send-mail',(req,res) => {
        })
    }else {
     email.send({
-        to:'upfain@qq.com',
-        title: 'test',
-        content: 'hahha'
+        to: req.query.to,
+        title: req.query.title,
+        content: req.query.content
     }).then(() => {
         res.json({
             code: 1,
