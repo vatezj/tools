d4a113920ab94f34ad2f70db3d7fa7d5694689bd
vate <2324757255@qq.com>
2017-10-13 18:01:35 +0800

    add keymap enter event

--- src/components/ping.vue
+++ src/components/ping.vue
@@ -2,13 +2,13 @@
 	<div class="mdui-container doc-container doc-no-cover pdl240">
 		<h1 class="doc-title mdui-text-color-theme">域名ip</h1>
 		<div class="mdui-container">
-			<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-4 mdui-col-lg-4 mdui-col-xl-4">
-				<input type="text" v-model="url" class="mdui-textfield-input">
-			</div>
-			<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-4 mdui-col-lg-4 mdui-col-xl-4">
+			<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-2 mdui-col-lg-2 mdui-col-xl-2">
 				<center>
 					<button class="mdui-btn mdui-color-theme-accent mdui-ripple" @click="ping()">ping</button>
 				</center>
+			</div>
+			<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-4 mdui-col-lg-4 mdui-col-xl-4">
+				<input type="text" v-model="url" class="mdui-textfield-input" @keyup.enter="ping">
 			</div>
 			<div class="mdui-col-xs-12 mdui-col-sm-12 mdui-col-md-4 mdui-col-lg-4 mdui-col-xl-4">
 				<input type="text" v-model="ip" class="mdui-textfield-input" >
